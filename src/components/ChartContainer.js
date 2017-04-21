1f7326e2008c0f3ab055d6fc3cdd8b3ab2e5f85d
Bryan Dickens <bigbrd1026@gmail.com>
2017-04-20 22:03:52 -0700

    cleaned formatting

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 // Note that Highcharts has to be required separately
 const ReactHighcharts = require('react-highcharts');
 const Highcharts3d = require('highcharts-3d');
@@ -15,115 +17,119 @@ HighchartsExporting(ReactHighcharts.Highcharts);
 
 const config = {
   // Set up the chart
-    chart: {
-      events: {
-        click: function () {
-          console.log("click!");
-        },
-        load: function () {
-          console.log("load!");
-        }
+  chart: {
+    events: {
+      click: function () {
+        console.log("click!");
       },
-      margin: 100,
-      type: 'scatter',
-      //zoomType: 'xy', if we want to zoom in
-      // panning: true,
-      // panKey: 'shift',
-      options3d: {
-        enabled: true,
-        alpha: 10,
-        beta: 50,
-        depth: 450,
-        viewDistance: 5,
-        fitToPlot: false,
-        frame: {
-          bottom: {
-            size: 1,
-            color: 'rgba(250,0,0,0.1)'
-          },
-          back: {
-            size: 1,
-            color: 'rgba(0,250,00,0.1)'
-          },
-          side: {
-            size: 1,
-            color: 'rgba(0,0,250,0.1)'
-          }
+      load: function () {
+        console.log("load!");
+      }
+    },
+    margin: 100,
+    type: 'scatter',
+    //zoomType: 'xy', if we want to zoom in
+    // panning: true,
+    // panKey: 'shift',
+    options3d: {
+      enabled: true,
+      alpha: 10,
+      beta: 50,
+      depth: 450,
+      viewDistance: 5,
+      fitToPlot: false,
+      frame: {
+        bottom: {
+          size: 1,
+          color: 'rgba(250,0,0,0.1)'
+        },
+        back: {
+          size: 1,
+          color: 'rgba(0,250,00,0.1)'
+        },
+        side: {
+          size: 1,
+          color: 'rgba(0,0,250,0.1)'
         }
       }
-    },
-    plotOptions: {
-      scatter: {
-        width: 10,
-        height: 10,
-        depth: 10
-      }
-    },
-    yAxis: {
-      min: 0,
-      max: 10,
-      title: null
-    },
-    xAxis: {
-      min: 0,
-      max: 10,
-      gridLineWidth: 1
-    },
-    zAxis: {
-      min: 0,
-      max: 10,
-      showFirstLabel: false
-    },
-    legend: {
-      enabled: false
-    },
+    }
+  },
+  plotOptions: {
+    scatter: {
+      width: 10,
+      height: 10,
+      depth: 10
+    }
+  },
+  yAxis: {
+    min: 0,
+    max: 10,
+    title: null
+  },
+  xAxis: {
+    min: 0,
+    max: 10,
+    gridLineWidth: 1
+  },
+  zAxis: {
+    min: 0,
+    max: 10,
+    showFirstLabel: false
+  },
+  legend: {
+    enabled: false
+  },
 
   series: [{
-      name: 'Data',
-      zIndex: 10,
-      colorByPoint: true,
-      //colors: ['#7cb5ec', '#434348', '#90ed7d']
-      marker: {
-        radius: 7,
-        symbol: 'circle'
-        //fillColor: {
-          //radialGradient: {
-            //cx: 0.7,
-            //cy: 0.3,
-            //r: 0.5
-          //},
-          //stops: [
-           // [0, 'rgba(15,5,5,1)'],
-            //[1, 'rgba(220,0,215,1)']
-          //]
-        //}
-      },
-      data: [
-        [9, 9, 1],
-        [8, 8, 2],
-        [7, 7, 3],
-        [6, 6, 4],
-        [5, 5, 5],
-        [4, 4, 6],
-        [3, 3, 7],
-        [2, 2, 8],
-        [1, 1, 9]
-      ]
-    }]
+    name: 'Data',
+    zIndex: 10,
+    colorByPoint: true,
+    //colors: ['#7cb5ec', '#434348', '#90ed7d']
+    marker: {
+      radius: 7,
+      symbol: 'circle'
+      //fillColor: {
+      //radialGradient: {
+      //cx: 0.7,
+      //cy: 0.3,
+      //r: 0.5
+      //},
+      //stops: [
+      // [0, 'rgba(15,5,5,1)'],
+      //[1, 'rgba(220,0,215,1)']
+      //]
+      //}
+    },
+    data: [
+      [9, 9, 1],
+      [8, 8, 2],
+      [7, 7, 3],
+      [6, 6, 4],
+      [5, 5, 5],
+      [4, 4, 6],
+      [3, 3, 7],
+      [2, 2, 8],
+      [1, 1, 9]
+    ]
+  }]
 };
 
 
 class ChartContainer extends Component {
-    // constructor(){
-    //     super();
-    //     this.state = {
-    //         chartFoo: 'x'
-    //     };
-
-    // }
+  constructor() {
+    super();
+    this.state = {
+      chartFoo: 'x'
+    };
+    this.click = this.click.bind(this);
+  }
   componentDidMount() {
     let chart = this.getChartFunc();
-    chart.series[0].addPoint({x: 10, y: 12, z: 3});
+    chart.series[0].addPoint({
+      x: 10,
+      y: 12,
+      z: 3
+    });
     // this.setState({chartState: chart});
     console.log(this);
     console.log(this.refs.chart);
@@ -135,33 +141,33 @@ class ChartContainer extends Component {
     return this.refs.chart.getChart();
   }
 
-   click(eStart) {
+  click(eStart) {
     console.log("hey");
     console.log(this.state.chartFoo);
     console.log(this.refs.chart.getChart());
-    console.log(this.chart);
-    
-    // let chart = chartContainer.type.Highcharts.charts[0]; //I hate this.. it should all be inside react not jquery
-    // eStart = chart.pointer.normalize(eStart);
-    // let posX = eStart.pageX,
-    //     posY = eStart.pageY,
-    //     alpha = chart.options.chart.options3d.alpha,
-    //     beta = chart.oxptions.chart.options3d.beta,
-    //     newAlpha,
-    //     newBeta,
-    //     sensitivity = 5; // lower is more sensitive
-    
+
+    let chart = this.refs.chart.getChart();
+    eStart = chart.pointer.normalize(eStart);
+    let posX = eStart.pageX,
+      posY = eStart.pageY,
+      alpha = chart.options.chart.options3d.alpha,
+      beta = chart.oxptions.chart.options3d.beta,
+      newAlpha,
+      newBeta,
+      sensitivity = 5; // lower is more sensitive
+
     function onMouseMove(e) {
-        console.log("mousemove");
-        // newBeta = beta + (posX - e.pageX) / sensitivity;
-        // chart.options.chart.options3d.beta = newBeta;
+      console.log("mousemove");
+      newBeta = beta + (posX - e.pageX) / sensitivity;
+      chart.options.chart.options3d.beta = newBeta;
 
-        // // Run alpha
-        // newAlpha = alpha + (e.pageY - posY) / sensitivity;
-        // chart.options.chart.options3d.alpha = newAlpha;
+      // Run alpha
+      newAlpha = alpha + (e.pageY - posY) / sensitivity;
+      chart.options.chart.options3d.alpha = newAlpha;
 
-        // chart.redraw(false);
+      chart.redraw(false);
     }
+
     function onMouseUp() {
       console.log("mouseup");
       document.removeEventListener("mousemove", onMouseMove);
@@ -170,11 +176,21 @@ class ChartContainer extends Component {
 
     document.addEventListener("mousemove", onMouseMove);
     document.addEventListener("mouseup", onMouseUp);
-    
+
   }
 
   render() {
-    return <ReactHighcharts config={config} ref="chart" domProps = {{id: 'chartie', onMouseDown:this.click}}/>;
+    return <ReactHighcharts config = {
+      config
+    }
+    ref = "chart"
+    domProps = {
+      {
+        id: 'chartie',
+        onMouseDown: this.click
+      }
+    }
+    />;
     // return <ReactHighcharts config={config} ref="chart" domProps = {{id: 'chartie'}}/>;
   }
 }

9a1ca9db8a774da8617b1d89d46528d5a6156cee
Bowden Kelly <bowdenk7@gmail.com>
2017-05-06 23:27:45 -0700

    can now publish to github pages

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,7 +18,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
+    publicPath: './',
     filename: 'bundle.js'
   },
   plugins: [
